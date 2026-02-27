9aa7bbf404183b940b8832fb02c3444645177b61
Ranjit-singh-786 <jiradhey402@gmail.com>
2026-02-27 20:59:30 +0530

    '27fab'

---
+++ learning_day5.py
@@ -0,0 +1,36 @@
+# CONDITIONAL STATEMEN 
+
+# st = "green" 
+# if st=="red":
+#     print("Plz stop  here")
+# else:
+#     print("GO from here")
+
+
+
+# st = "green" 
+# if st=="red":
+#     pass 
+# else:
+#     print("GO from here")
+
+
+
+
+
+# st = input("plz enter signal color : ")
+# if st=="red":
+#     print("Plz stop  here")
+# elif st== "yellow":
+#     print("plz be ready")
+# else:
+#     print("GO from here")
+
+
+
+
+## LOOP 
+for i in range(5):
+    print(i," welcome to all of you at techie solution")
+
+
