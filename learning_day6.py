f575d51dd7aa4f586c358a5fbcc718c67679e9e3
Ranjit-singh-786 <jiradhey402@gmail.com>
2026-03-02 23:04:28 +0530

    'day6'

---
+++ learning_day6.py
@@ -0,0 +1,59 @@
+# for i in range(1,6):
+#     print(i)
+
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i + 1 
+ 
+
+
+# password = ""
+# while password != "claude1234":  
+#     password = input("plz enter password :  ") 
+# print("access granted")
+
+
+# for i in range(1,25):
+#     print(i)
+#     if i == 10:
+#         break 
+
+
+
+# for i in range(1,25):
+#     if i == 10:
+#         continue  
+#     print(i)
+
+
+
+def sum_of_n_number(number):
+    total_sum = 0
+    for i in range(1,number+1):
+        total_sum = total_sum + i  
+    return total_sum
+
+
+# sum_of_n_number(number=5)   # calling 
+# sum_of_n_number(number=10)   # calling 
+# sum_of_n_number(3)
+
+
+def sum_of_two(num1,num2):
+    total_sum = num1 + num2 
+    return total_sum
+
+# n1 = int(input("enter first number : "))
+# n2 = int(input("enter second number : "))
+# output = sum_of_two(num1=n1,num2=n2)  # calling 
+# output = sum_of_two(5,3)  # calling 
+# output = sum_of_two(num2=3,num1=5)  # calling 
+# print(output)
+
+
+
+print(4**2)
+
+
